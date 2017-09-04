f95162228d9a9e7216330b1f9174347624b5cff4
ltakuno <leonardo.takuno@gmail.com>
2017-09-04 12:53:37 -0300

    Controlador de Clientes

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_test.Controllers
+{
+
+    [Route("api/produtos")]
+    public class ClientesController : Controller
+    {
+        public static List<Cliente> vetor = new List<Cliente>();
+        
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Cliente> Get()
+        {
+            return vetor;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Cliente Get(int id)
+        {
+            foreach(var i in vetor){
+                if(i.codigo == id){
+                    return i;
+                }
+            }
+            return vetor[0];
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IEnumerable<Cliente> Post([FromBody]Cliente cliente)
+        {
+            vetor.Add(new Cliente(cliente.nome, cliente.cpf, cliente.endereco));
+            return vetor;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
